804daed86cab406b99644c33e5d75b1fff5f9d4e
Riley Park <rileysebastianpark@gmail.com>
2025-06-28 16:12:00 -0700

    build: publish using fill (#1599)

--- proxy/build.gradle.kts
+++ proxy/build.gradle.kts
@@ -1,10 +1,12 @@
 import com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCacheFileTransformer
+import io.papermc.fill.model.BuildChannel
 
 plugins {
     application
     id("velocity-init-manifest")
     id("velocity-publish")
     alias(libs.plugins.shadow)
+    alias(libs.plugins.fill)
 }
 
 application {
@@ -106,6 +108,24 @@ tasks {
     named<JavaExec>("run") {
         workingDir = file("run").also(File::mkdirs)
         standardInput = System.`in` // Doesn't work?
+    }
+}
+
+val projectVersion = version as String
+fill {
+    project("velocity")
+
+    build {
+        channel = BuildChannel.STABLE
+        versionFamily("3.0.0")
+        version(projectVersion)
+
+        downloads {
+            register("server:default") {
+                file = tasks.shadowJar.flatMap { it.archiveFile }
+                nameResolver.set { project, _, version, build -> "$project-$version-$build.jar" }
+            }
+        }
     }
 }
 
