2f130ab4d0147e6dbcda5591d50749b219bce82c
Nassim Jahnke <nassim@njahnke.dev>
2026-01-31 11:34:51 +0100

    Promote build channel to stable

--- proxy/build.gradle.kts
+++ proxy/build.gradle.kts
@@ -116,7 +116,7 @@ fill {
     project("velocity")
 
     build {
-        channel = BuildChannel.BETA
+        channel = BuildChannel.STABLE
         versionFamily("3.0.0")
         version(projectVersion)
 
